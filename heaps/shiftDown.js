7b5f1778d9f81f1aab4c33bf82cc4c287163139f
Anubhav Tyagi <64315739+anubhavtyagi14@users.noreply.github.com>
2022-08-04 15:17:33 +0530

    refactoring

--- heaps/shiftDown.js
+++ heaps/shiftDown.js
@@ -9,23 +9,24 @@ function siftDown(idx) {
     // it has to at least have a left child
     if(leftChildIdx >= heap.length) return;
     // start at the current node and find the new min-value among the three nodes (parent, left child, right child)
-    let node = heap[idx];
+    // let node = heap[idx];
     let minValueIdx = idx;
     // check the left child
-    if(heap[leftChildIdx] !== null && node > heap[leftChildIdx]) {
-      node = heap[leftChildIdx];
+    if(heap[leftChildIdx] !== null && heap[minValueIdx] > heap[leftChildIdx]) {
+      // node = heap[leftChildIdx];
       minValueIdx = leftChildIdx;
     }
     // now check the right child
-    if(heap[rightChildIdx] !== null && node > heap[rightChildIdx]) {
-      node = heap[rightChildIdx];
+    if(heap[rightChildIdx] !== null && heap[minValueIdx] > heap[rightChildIdx]) {
+     // node = heap[rightChildIdx];
       minValueIdx = rightChildIdx;
     }
     // if the min-value is not the initial node, do the swap and then recursion
     if(minValueIdx !== idx) {
-      let temp = heap[idx];
-      heap[idx] = heap[minValueIdx];
-      heap[minValueIdx] = temp;
+      // let temp = heap[idx];
+      // heap[idx] = heap[minValueIdx];
+      // heap[minValueIdx] = temp;
+      [heap[idx],heap[minValueIdx]] = [heap[minValueIdx],heap[idx]]
       siftDown(minValueIdx);
     }
   }
